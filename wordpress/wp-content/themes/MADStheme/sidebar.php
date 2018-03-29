5db660d88f6efc3c5c40a6c5e43e08c2d7bd1841
dacruzmsebastien <dacruzmsebastien@gmail.com>
2018-03-29 15:33:04 +0200

    ajout bdd

--- wordpress/wp-content/themes/MADStheme/sidebar.php
+++ wordpress/wp-content/themes/MADStheme/sidebar.php
@@ -4,31 +4,28 @@
     <ul>
         <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?> CONTENU DE LA SIDEBAR
 
-        <li id="search">
-            <?php include(TEMPLATEPATH . '/searchform.php'); ?>
+        <!-- <li id="search">
+            <//?php include(TEMPLATEPATH . '/searchform.php'); ?>
         </li>
-<<<<<<< HEAD
-=======
 
->>>>>>> 5fb8b39d00082fbf3d9904d9614f714ae15eec85
         <li>
             <h2>Categories</h2>
-            <?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
-            <?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>
+            <//?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
+            <//?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>
         </li>
         <li>
             <h2>Archives</h2>
             <ul>
-                <?php wp_get_archives('type=monthly'); ?>
-                <?php get_links_list(); ?>
+                <//?php wp_get_archives('type=monthly'); ?>
+                <//?php get_links_list(); ?>
             </ul>
         </li>
         <li>
             <h2>Infos Meta</h2>
             <ul>
-                <?php wp_register(); ?>
+                <//?php wp_register(); ?>
                 <li>
-                    <?php wp_loginout(); ?>
+                    <//?php wp_loginout(); ?>
                 </li>
                 <li>
                     <a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">
@@ -46,20 +43,28 @@
                 <li>
                     <a href="http://wordpress-fr.net/" title="Communauté française de WordPress et WPmu.">WordPress Francophone</a>
                 </li>
-                <?php wp_meta(); ?>
+                <//?php wp_meta(); ?>
             </ul>
         </li>
         <li>
             <h2>Abonnez-vous au blog !</h2>
             <ul>
                 <li>
-                    <a href="<?php bloginfo('rss2_url'); ?>" title="Flux RSS des articles">Flux RSS des articles</a>
+                    <a href="<//?php bloginfo('rss2_url'); ?>" title="Flux RSS des articles">Flux RSS des articles</a>
                 </li>
                 <li>
-                    <a href="<?php bloginfo('comments_rss2_url'); ?>" title="Flux RSS des commentaires">Flux RSS des commentaires</a>
+                    <a href="<//?php bloginfo('comments_rss2_url'); ?>" title="Flux RSS des commentaires">Flux RSS des commentaires</a>
                 </li>
             </ul>
-        </li>
+        </li> -->
+        <div class="liensSubscribe">
+            <ul>
+                <li class="fcbk"><a href="https://fr-fr.facebook.com/"><img src="http://localhost/MADS-foodog/wordpress/wp-admin/images/facebook.png" alt="facebook"></a>21 562 Fans LIKE</li>
+                <li class="twitter"><a href="https://twitter.com/?lang=fr"><img src="http://localhost/MADS-foodog/wordpress/wp-admin/images/twitter.png" alt ="twitter"></a>1 401 Followers FOLLOW</li>
+                <li class="insta"><a href="https://www.instagram.com/?hl=fr"><img src="http://localhost/MADS-foodog/wordpress/wp-admin/images/insta.png" alt="instagram"></a>23 538 Followers FOLLOW</li>
+                <li><img class="alignleft size-full wp-image-133" src="http://localhost/MADS-foodog/wordpress/wp-content/uploads/2018/03/sidebar1.jpg" alt="" width="250" height="500" /></li>
+            </ul>
+        </div>
         <?php endif; ?>
     </ul>
 </div>
