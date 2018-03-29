52070176e645dd4253747c8150c85bd0c55a11c6
dacruzmsebastien <dacruzmsebastien@gmail.com>
2018-03-29 11:57:25 +0200

    category name above title

--- wordpress/wp-content/themes/MADStheme/archive.php
+++ wordpress/wp-content/themes/MADStheme/archive.php
@@ -7,24 +7,23 @@
                 <?php while(have_posts()) : the_post(); ?>
                 <div class="post" id="post-<?php the_ID(); ?>">
                     <h2>
+                        <p class="postmetadata">
+                            <?php // the_time('j F Y') ?> <!--par -->
+                            <!-- <?php // the_author() ?> | Cat&eacute;gorie: -->
+                            <?php the_category(', ') ?>
+                            <?php // comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires'); ?>
+                            <?php // edit_post_link('Editer', ' &#124; ', ''); ?>
+                        </p>
                         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
                           <?php
-                          add_theme_support( 'post-thumbnails', array( 'post' ) ); 
+                          add_theme_support( 'post-thumbnails', array( 'post' ) );
                           // Ajouter les images à la une sur les articles uniquement
                            the_post_thumbnail();
                             // affiche la miniature par défaut
                             // ?>
                             <?php the_title(); ?>
-
                         </a>
                     </h2>
-                    <p class="postmetadata">
-                        <?php the_time('j F Y') ?> par
-                        <?php the_author() ?> | Cat&eacute;gorie:
-                        <?php the_category(', ') ?> |
-                        <?php comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires'); ?>
-                        <?php edit_post_link('Editer', ' &#124; ', ''); ?>
-                    </p>
                     <div class="post_content">
                         <?php the_excerpt(); ?>
                     </div>
